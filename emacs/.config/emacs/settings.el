f23bc010b7b8ff0533fe12c058dd9db54319ccd1
Alex Ozer <thealexozer@gmail.com>
2017-08-25 00:04:15 -0400

    Update 08/25/17

---
+++ emacs/.config/emacs/settings.el
@@ -0,0 +1,14 @@
+(condition-case nil
+    (progn
+      (add-to-list 'default-frame-alist '(font . "Monaco"))
+      (set-face-attribute 'default nil :font "Monaco")
+      (set-face-attribute 'default (selected-frame) :height 110))
+  (error nil))
+
+(require 'package)
+(setq package-enable-at-startup nil)
+;(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+;(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+;(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
