5dc9335938ae04e2fcdd6a81d895e2a84ab9528a
Alex Ozer <alexozer@users.noreply.github.com>
2018-09-16 12:30:32 -0400

    Update polybar icons to nerd font
    Because ionicons updated and I can't find the new unicode codes...

--- polybar/.config/polybar/mpris.py
+++ polybar/.config/polybar/mpris.py
@@ -7,8 +7,8 @@ import gi
 gi.require_version('Playerctl', '1.0')
 from gi.repository import Playerctl, GLib
 
-MUSIC_ICON = ''
-PAUSE_ICON = ''
+MUSIC_ICON = ''
+PAUSE_ICON = ''
 
 
 class PlayerStatus:
