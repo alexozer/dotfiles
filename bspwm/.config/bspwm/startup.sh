a2a365462049a95391e6d98f2de988affab0b965
Alex Ozer <thealexozer@gmail.com>
2019-11-17 10:44:17 -0500

    Start backup script with systemd timer
    I tried these a long time ago but ran into issues, let's try this again.
    I won't always power my computer off at night, so it's important to run
    the backup script not just on login.

--- bspwm/.config/bspwm/startup.sh
+++ bspwm/.config/bspwm/startup.sh
@@ -12,22 +12,15 @@ gnome-terminal &
 xfsettingsd &
 xfce4-notifyd &
 xfdesktop --disable-wm-check &
-
 thunar --daemon &
-CM_SELECTIONS="clipboard" clipmenud &
-
 steam -silent &
 discord &
 timetrack &
-# redshift-gtk &
 dropbox &
-
-# Try to start new sessions; if they already exist, oh well
 tmux new-session -d -s weechat weechat &
-tmux new-session -d -s backup backup &
-
 dex -a &
-
 udiskie --no-notify --tray &
+# redshift-gtk &
+# CM_SELECTIONS="clipboard" clipmenud &
 
 sleep 1 && polybar top --reload &
