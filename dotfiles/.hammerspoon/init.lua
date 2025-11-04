518b7e0103b0cbe09c37ad93b8f18b21dfcec7e5
Alex Ozer <alexozer@users.noreply.github.com>
2025-11-04 17:40:57 -0500

    Hammerspoon raise/run config

---
+++ dotfiles/.hammerspoon/init.lua
@@ -0,0 +1,11 @@
+hs.hotkey.bind({"alt"}, "u", function()
+    hs.application.launchOrFocus("Google Chrome")
+end)
+
+hs.hotkey.bind({"alt"}, "i", function()
+    hs.application.launchOrFocus("Visual Studio Code")
+end)
+
+hs.hotkey.bind({"alt"}, "o", function()
+    hs.application.launchOrFocus("Ghostty")
+end)
