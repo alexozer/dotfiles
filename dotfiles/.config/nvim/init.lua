65ffcf02f6b1d5d117f13207ad2172eeb9bb5286
Alex Ozer <alexozer@users.noreply.github.com>
2026-02-16 10:24:49 -0500

    Compensate for neovim dumbness

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -112,7 +112,7 @@ vim.pack.add({
     { src = "https://github.com/nvim-lua/plenary.nvim" },
     { src = "https://github.com/nvim-telescope/telescope.nvim" },
     { src = "https://github.com/windwp/nvim-autopairs" },
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "master" },
 })
 require('gitsigns').setup({
   signs = {
