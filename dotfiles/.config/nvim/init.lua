8555ab150409ec1b9af29507649d82c6b82394c5
Alex Ozer <alexozer@users.noreply.github.com>
2026-02-19 11:50:00 -0500

    Slight better harpoon mappings

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -203,8 +203,8 @@ vim.api.nvim_create_autocmd('FileType', {
 
 local harpoon = require('harpoon')
 harpoon:setup()
-vim.keymap.set("n", "<leader>p", function() harpoon:list():add() end)
-vim.keymap.set("n", "<C-'>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<leader>p", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "gp", function() harpoon:list():add() end)
 vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<C-l>", function() harpoon:list():select(3) end)
